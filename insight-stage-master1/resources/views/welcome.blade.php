1bf640489d7cea2c8fbdf9c4c6517e9944f5ff85
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-07-18 19:17:51 +0100

    novo

--- insight-stage-master1/resources/views/welcome.blade.php
+++ insight-stage-master1/resources/views/welcome.blade.php
@@ -46,6 +46,7 @@
 
             .content {
                 text-align: center;
+                margin:auto;
             }
 
             
@@ -100,19 +101,43 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+          </div>
+      
 
 		
-			<div class="container team4" style="max-width:700px">							
-				<div class="row">
+			<div class="container team4" style="max-width:1200px;margin-right:auto;justify-content:center;margin: auto;">							
+				<div class="row" >
       <!-- column  -->
-      <div class="col-lg-3 mb-4">
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
         <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:170px;padding-right:28px;justify-content:center;" />
+        <div class="row" style="margin-right:80px;justify-content:center;">
+          <div class="col-md-12" >
+            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;" />
           </div>
-          <div class="col-md-12 text-center">
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
+              <h6 class="subtitle mb-3">Property Specialist</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+        <!-- column  -->
+        <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="margin-right:80px;justify-content:center;">
+          <div class="col-md-12" >
+            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;" />
+          </div>
+          <div class="col-md-12 text-center" >
             <div class="pt-2">
               <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
               <h6 class="subtitle mb-3">Property Specialist</h6>
@@ -128,20 +153,21 @@
         <!-- Row -->
       </div>
       <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
+       <!-- column  -->
+       <div class="col-lg-3 mb-4"style="margin:auto;" >
         <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t2.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:170px;padding-right:28px;" />
+        <div class="row" style="margin-right:80px;justify-content:center;">
+          <div class="col-md-12" >
+            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;" />
           </div>
-          <div class="col-md-12 text-center">
+          <div class="col-md-12 text-center" >
             <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
+              <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
               <h6 class="subtitle mb-3">Property Specialist</h6>
               <ul class="list-inline">
                 <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
                 <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-paper-plane"></i></a></li>
+               
                 
               </ul>
             </div>
@@ -150,20 +176,21 @@
         <!-- Row -->
       </div>
       <!-- column  -->
-     <!-- column  -->
-     <div class="col-lg-3 mb-4">
+       <!-- column  -->
+       <div class="col-lg-3 mb-4"style="margin:auto;" >
         <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:170px;padding-right:28px;justify-content:center;" />
+        <div class="row" style="margin-right:80px;justify-content:center;">
+          <div class="col-md-12" >
+            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;" />
           </div>
-          <div class="col-md-12 text-center">
+          <div class="col-md-12 text-center" >
             <div class="pt-2">
               <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
               <h6 class="subtitle mb-3">Property Specialist</h6>
               <ul class="list-inline">
-              <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
                 <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-paper-plane"></i></a></li>
+               
                 
               </ul>
             </div>
@@ -172,42 +199,21 @@
         <!-- Row -->
       </div>
       <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
+        <!-- column  -->
+        <div class="col-lg-3 mb-4"style="margin:auto;" >
         <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:170px;padding-right:28px;justify-content:center;" />
+        <div class="row" style="margin-right:80px;justify-content:center;">
+          <div class="col-md-12" >
+            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;" />
           </div>
-          <div class="col-md-12 text-center">
+          <div class="col-md-12 text-center" >
             <div class="pt-2">
               <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
               <h6 class="subtitle mb-3">Property Specialist</h6>
               <ul class="list-inline">
-              <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
                 <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-paper-plane"></i></a></li>
-                
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Row -->
-      </div>
-      <!-- column  -->
-      <!-- column  -->
-      <div class="col-lg-3 mb-4">
-        <!-- Row -->
-        <div class="row">
-          <div class="col-md-12">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:170px;padding-right:28px;" />
-          </div>
-          <div class="col-md-12 text-center">
-            <div class="pt-2">
-              <h5 class="mt-4 font-weight-medium mb-0" >Michael Doe</h5>
-              <h6 class="subtitle mb-3">Property Specialist</h6>
-              <ul class="list-inline">
-              <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-linkedin"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-paper-plane"></i></a></li>
+               
                 
               </ul>
             </div>
@@ -218,7 +224,8 @@
       <!-- column  -->
 						</div>	
 						</div>
-						</div>
+					
+            </div>
     </div>
     </body>
 </html>
