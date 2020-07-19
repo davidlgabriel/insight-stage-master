8911eef489556b9ef97a956f1a75d560149950b5
David Gabriel <david.g.leal00@gmail.com>
2020-07-19 20:05:29 +0100

    slider

--- insight-stage-master1/public/js/scripts.js
+++ insight-stage-master1/public/js/scripts.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function(){
-    $('.post-wrapper').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: $('.next'),
-        prevArrow: $('.prev'),
-
-    });
-});
-*/
-
 /*
     Carousel
 */
