c7355e2a68d87bb1f42cb19a7f63e85e1a4c17e2
David Gabriel <david.g.leal00@gmail.com>
2020-07-19 16:02:05 +0100

    slider

--- insight-stage-master1/resources/views/home.blade.php
+++ insight-stage-master1/resources/views/home.blade.php
@@ -1,70 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="page-wrapper">
-    <div class="post-slider">
-        <h1 class="slider-title"><a href="">Trending Posts</a></h1>
-        <i class="fas fa-chevron-left prev"></i>
-        <i class="fas fa-chevron-right next"></i>
-        <div class="post-wrapper">
-            <div class="post">
-                <img src="{{ asset('img/23.jpeg') }}" alt="" class="slider-image">
-                <div class="post-info">
-                    <h4><a href="">This is the Title of the post</a></h4>
-                    <p>In this description we talk about a lot of finance and economy things,
-                    that no one let use speak about in the newspaper</p>
-                    <i class="far fa-user-circle">Nuno Dias </i>
-                    &nbsp;
-                    <i class="far fa-calendar-alt"> July 15, 2020</i>
+
+<section>
+<div class="top-content">
+    <div class="container-fluid">
+        <h1 class="slider-title">Trending Posts</h1>
+        <div id="carousel-example" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner row w-100 mx-auto" role="listbox">
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active" >
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                            <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                               
+                            </div>
+                    </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                    </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                    </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the caquick example text to build on quick example text to build on rd title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                    </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="{{ asset('img/23.jpeg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <a class="card-title font-weight-bold" href=""><h4 class="card-title font-weight-bold">Card title</h4></a>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                            </div>
+                    </div>
                 </div>
             </div>
-            <div class="post">
-                <img src="{{ asset('img/23.jpeg') }}" alt="" class="slider-image">
-                <div class="post-info">
-                    <h4><a href="">This is the Title of the post</a></h4>
-                    <p>In this description we talk about a lot of finance and economy things,
-                    that no one let use speak about in the newspaper</p>
-                    <i class="far fa-user-circle">John Afonso </i>
-                    &nbsp;
-                    <i class="far fa-calendar-alt"> July 15, 2020</i>
-                </div>
-            </div>
-            <div class="post">
-                <img src="{{ asset('img/23.jpeg') }}" alt="" class="slider-image">
-                <div class="post-info">
-                    <h4><a href="">This is the Title of the post</a></h4>
-                    <p>In this description we talk about a lot of finance and economy things,
-                    that no one let use speak about in the newspaper</p>
-                    <i class="far fa-user-circle">Ricardo Salgado </i>
-                    &nbsp;
-                    <i class="far fa-calendar-alt"> July 15, 2020</i>
-                </div>
-            </div>
-            <div class="post">
-                <img src="{{ asset('img/23.jpeg') }}" alt="" class="slider-image">
-                <div class="post-info">
-                    <h4><a href="">This is the Title of the post</a></h4>
-                    <p>In this description we talk about a lot of finance and economy things,
-                    that no one let use speak about in the newspaper</p>
-                    <i class="far fa-user-circle">Manuel Moura Guedes </i>
-                    &nbsp;
-                    <i class="far fa-calendar-alt"> July 15, 2020</i>
-                </div>
-            </div>
-            <div class="post">
-                <img src="{{ asset('img/23.jpeg') }}" alt="" class="slider-image">
-                <div class="post-info">
-                    <h4><a href="">This is the Title of the post</a></h4>
-                    <p>In this description we talk about a lot of finance and economy things,
-                    that no one let use speak about in the newspaper/p>
-                    <i class="far fa-user-circle">Cavaco Silva </i>
-                    &nbsp;
-                    <i class="far fa-calendar-alt"> July 15, 2020</i>
-                </div>
-            </div>
-        </div>
+            <a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" style="color=black;" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>        
     </div>
 </div>
+<div class="vertical-center" style="text-align: center;">
+  <button class="button button4 font-weight-bold">Ver Mais</button>
+</div>
 
-
+</section>
 @endsection
