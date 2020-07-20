036bb378cb264311b6b17fbe4ccd84486a26090a
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-07-20 11:49:38 +0100

    members section(members.css, etc)

--- insight-stage-master1/resources/views/home.blade.php
+++ insight-stage-master1/resources/views/home.blade.php
@@ -84,4 +84,202 @@
 </div>
 
 </section>
+
+<section id="members">
+
+
+<div class="container team4" style="max-width:1200px;margin-right:auto;justify-content:center;margin: auto;">	
+    <h1 class="slider-title" style="line-height:300px;">Our Team</h1>						
+	<div class="row" >
+      <!-- column  -->
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e0.365dm.com/f1/drivers/h_full_809.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Lewis Hamilton</h5>
+              <h6 class="subtitle mb-3">Mercedes-AMG-Petronas</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+      <!-- column  -->
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://www.f1fantasytracker.com/Images/Drivers/Bottas.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Valteri Bottas</h5>
+              <h6 class="subtitle mb-3">Mercedes-AMG-Petronas</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+       <!-- column  -->
+       <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e2.365dm.com/f1/drivers/h_full_1489.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Charles Leclerc</h5>
+              <h6 class="subtitle mb-3">Ferrari</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+       <!-- column  -->
+       <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e2.365dm.com/f1/drivers/h_full_1465.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Max Verstappen</h5>
+              <h6 class="subtitle mb-3">RedBull-Aston Martin</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+       <!-- column  -->
+       <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e2.365dm.com/f1/drivers/h_full_1501.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Lando Norris</h5>
+              <h6 class="subtitle mb-3">McLaren-Renault</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+
+      <!-- column  -->
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e1.365dm.com/f1/drivers/h_full_1475.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Carlos Sainz</h5>
+              <h6 class="subtitle mb-3">McLaren-Renault</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+      <!-- column  -->
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e2.365dm.com/f1/drivers/h_full_1471.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Esteban Ocon</h5>
+              <h6 class="subtitle mb-3">Renault</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+      <!-- column  -->
+      <div class="col-lg-3 mb-4"style="margin:auto;" >
+        <!-- Row -->
+        <div class="row" style="justify-content:center;">
+          <div class="col-md-12 text-center" >
+            <img src="https://e1.365dm.com/f1/drivers/h_full_1504.png" alt="wrapkit" class="img-fluid rounded-circle" style="max-width:200px;padding-right:28px;margin-left:25px;" />
+          </div>
+          <div class="col-md-12 text-center" >
+            <div class="pt-2">
+              <h5 class="mt-4 font-weight-medium mb-0" >Alexander Albon</h5>
+              <h6 class="subtitle mb-3">RedBull-Aston Martin</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-social-linkedin"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1" style="line-height:12px;"><i class="icon-paper-plane"></i></a></li>
+               
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Row -->
+      </div>
+      <!-- column  -->
+    </div>	
+    </div>
+
+<section>
 @endsection
+
+
--- insight-stage-master1/resources/views/layouts/app.blade.php
+++ insight-stage-master1/resources/views/layouts/app.blade.php
@@ -10,11 +10,16 @@
 
     <title>IS</title>
 
+    <!-- Icons -->
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+
     <!-- Scripts 
     <script src="{{ asset('js/app.js') }}" defer></script>-->
     
     <script defer src="https://friconix.com/cdn/friconix.js"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
+					
     <!-- Fonts -->
    
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -30,6 +35,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/slider.css') }}" rel="stylesheet">
     <link href="{{ asset('css/hamburgers.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/members.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 </head>
 <body>
