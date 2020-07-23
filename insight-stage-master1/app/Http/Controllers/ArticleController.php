abb049b11ceff0afee1ddaef5825544440c08666
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-07-23 18:21:08 +0100

    Update ArticleController.php

--- insight-stage-master1/app/Http/Controllers/ArticleController.php
+++ insight-stage-master1/app/Http/Controllers/ArticleController.php
@@ -2,29 +2,111 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    protected $fillabel = ['message','file','image'];
     /**
-     * Create a new controller instance.
+     * Display a listing of the resource.
      *
-     * @return void
-     */
-    /*
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    */
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('articlespage');
+        $file=Article::all();
+        return view('allarticle', compact('file'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('article');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data=new Article;
+        if($request->message == null){
+            $data->message="";
+        }
+        $data->name=$request->name;
+        $data->email=$request->email;
+        if($request->message != null){
+        $data->message=$request->message;
+        }
+        if ($request->file('file')) {
+            # code...
+            $file=$request->file('file');
+            $filename=time().'.'.$file->getClientOriginalExtension();
+            $request->file->move('storage/',$filename);
+            
+            $data->file= $filename;
+        }
+        $data->image="";
+        $data->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data=Article::find($id);
+        return view('file', compact('data'));
+    }
+
+    public function download($file)
+    {
+        return response()->download('storage/'.$file);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        //
     }
 }
