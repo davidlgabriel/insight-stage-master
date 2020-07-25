9d2db68da0afec848f3c397bcac7c3141fd040b1
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-07-25 20:50:56 +0100

    Update

--- insight-stage-master1/resources/views/article.blade.php
+++ insight-stage-master1/resources/views/article.blade.php
@@ -3,37 +3,64 @@
 @section('content')
 
 
-<div class="container">
+<section class="submit">
+    <div class="container">
+        <div class="title" >
+            <div style="margin-bottom:6%;">
+             <h1> Submit your Article </h1>
+            </div>
+        <div>
+        
+        <div class="form">
+            <form action="/article" method="POST" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <div class="row">
+                    
+                    <div class="">
+                        <div class="form-group" >
+                            <label for="exampleInputEmail1" style="line-height:30px;">Name</label>
+                        </div>
+                        <div class="form-group clor">
+                            <input type="text" name="name"  required>
+                    </div>
 
-    <div class="submit-title" >
-            <h1> Submit your Article </h1>
-    <div>
-    <div class="submit-form">
-        <form action="/article" method="POST" enctype="multipart/form-data">
-        {{csrf_field()}}
-        <div class="form-group">
-        <label for="exampleInputEmail1">Name</label>
-            <input type="text" name="name" placeholder="name" required>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <div class="">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="email" required>
+                        </div>
+                    </div>
+
+                    <div class="">
+                        <div class="form-group">
+                            <label class="file">
+                            <input type="file" name="file" required>
+                            Add a file
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="" style="margin-left:60%;margin-top:0%;line-height:200%;">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Message</label>
+                        </div>
+                        <div class="form-group" >
+                            <input type="text" name="message" value="" style="padding-top:47%;width:210%;">
+                        
+                        </div>
+                        <div class="">
+                            <input type="submit" value="Submit">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        
         </div>
-        <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="text" name="email" placeholder="email" required>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-        <label for="exampleInputEmail1">Message</label>
-        <input type="text" name="message" placeholder="message" value="" >
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">File</label>
-            <input type="file" name="file" required>
-        </div>
-        <input type="submit" value="Submit">
-        </form>
     </div>
-</div>
+</section>
 
 
 @endsection
