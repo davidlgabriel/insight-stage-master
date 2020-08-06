c8e1fd61ccfa26382ab4252d9005dd7ec309793e
David Gabriel <david.g.leal00@gmail.com>
2020-08-06 14:45:13 +0100

    route

--- insight-stage-master1/routes/web.php
+++ insight-stage-master1/routes/web.php
@@ -14,21 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PageController@index')->name('home');
-
-//ArticlesPage
-Route::get('/articles','PageController@articles')->name('articles');
- 
+Route::post('/', 'ArticleController@store');
+Route::get('/', 'ArticleController@indexhome');
 
 
 //Article
 Route::get('/article/submit', 'ArticleController@create');
 Route::post('/article', 'ArticleController@store');
 
+
 //all
-Route::get('/article', 'ArticleController@index');
+Route::get('/article', 'ArticleController@indexadmin');
 Route::get('/article/all/{id}','ArticleController@show');
 Route::get('/article/download/{file}','ArticleController@download');
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
