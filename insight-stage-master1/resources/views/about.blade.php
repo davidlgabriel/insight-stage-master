631dc917f7fe883827116096a10bd275c70fe678
David Gabriel <david.g.leal00@gmail.com>
2020-08-07 17:17:32 +0100

    about

--- insight-stage-master1/resources/views/about.blade.php
+++ insight-stage-master1/resources/views/about.blade.php
@@ -2,4 +2,41 @@
 
 @section('content')
 
+<div class="container-fluid">
+    <div class="row" style="padding-left: 50px; padding-right: 50px">
+        <div class="column">
+            <h4>Missão do IS:</h4>
+            <p>
+                Nesta página vamos falar sobre cenas à toa de várias
+                disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+                Nesta página vamos falar sobre cenas à toa
+                de várias disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+                Nesta página vamos falar sobre cenas à toa
+                de várias disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+            </p>
+        </div>
+        <div class="column">
+            <h4>Os nossos valores:</h4>
+            <p>
+                Nesta página vamos falar sobre cenas à toa
+                de várias disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+                Nesta página vamos falar sobre cenas à toa
+                de várias disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+                Nesta página vamos falar sobre cenas à toa
+                de várias disciplinas e ciências, bla bla bla cenas de meninos de gestão
+                bla bla bla marketing aqui um pouco bla bla bla.
+            </p>
+        </div>
+
+    </div>
+
+
+
+</div>
+
 @endsection
